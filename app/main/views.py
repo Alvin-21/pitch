27506298908d2d0db1b1e3b0b49c7209cb2b33e6
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-24 14:53:39 +0300

    Add app structure logic.

---
+++ app/main/views.py
@@ -0,0 +1,13 @@
+from flask import render_template, request, redirect, url_for
+from . import main
+
+# Views
+@main.route('/')
+def index():
+    '''
+    Function that returns the index page and its data.
+    '''
+
+    title = 'Home'
+
+    return render_template('index.html', title=title)
