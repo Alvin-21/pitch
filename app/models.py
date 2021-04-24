4a7b828c78459c3c19793c67521d60990b3a5b01
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-24 19:56:13 +0300

    Add logic for defining user objects.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,16 @@
+from . import db
+
 class User:
     """
+    This class defines new User objects.
     """
+
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
 
 
 class Comment:
