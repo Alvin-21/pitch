0b7746607ca3649e7b91ea7ab31845497e4608e0
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-24 20:43:29 +0300

    Add database migrations.

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://Alvin:monkey@localhost/watchlist'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://Alvin:monkey@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 from app import create_app, db
 from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
 from app.models import User
 
 # Creating app instance
@@ -7,6 +8,8 @@ app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server', Server)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def test():
@@ -14,7 +17,7 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
-    
+
 
 @manager.shell
 def make_shell_context():
