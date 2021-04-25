6141b4da29b7362456d8e405c46849b3079a5d72
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-25 16:04:50 +0300

    Add columns for bio and profile pic path.

--- app/models.py
+++ app/models.py
@@ -17,6 +17,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(255), index=True)
     pass_secure = db.Column(db.String(255))
     email = db.Column(db.String(255), unique=True, index=True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
 
     @property
     def password(self):
