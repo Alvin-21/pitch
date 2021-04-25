12be143d07fef8bf3f4ac335011ce2a7a5fccf07
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-25 17:43:57 +0300

    Add classes for pitch and comment forms.

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, SelectField
+from wtforms import StringField, TextAreaField, SubmitField, RadioField
 from wtforms.validators import Required
 
 
@@ -7,6 +7,23 @@ class UpdateProfile(FlaskForm):
     """
     Class for updating profile bio.
     """
-    
+
     bio = TextAreaField('Tell us about you.', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+    """
+    Class for providing the pitch.
+    """
+
+    category = RadioField('Label', choices=[('pickup lines', 'pickup lines'), ('interview pitch','interview pitch'), ('product pitch', 'product pitch'), ('promotion pitch', 'promotion pitch')])
+    pitch = TextAreaField("Please provide your pitch.", validators=[Required()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    """
+    Class for providing the comment for a pitch.
+    """
+
+    comment = TextAreaField('Type your comment',validators=[Required()])
     submit = SubmitField('Submit')
