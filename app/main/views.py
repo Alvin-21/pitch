16b27db67c4507de907d2f869c4bc1a405243210
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-25 21:17:12 +0300

    Add function for returning pitches page.

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, request, redirect, url_for, abort
 from . import main
-from flask_login import login_required
-from ..models import User
-from .forms import UpdateProfile
+from flask_login import login_required, current_user
+from ..models import User, Pitch, Comment
+from .forms import UpdateProfile, PitchForm, CommentForm
 from .. import db
 
 # Views
@@ -15,6 +15,26 @@ def index():
     title = 'Home'
 
     return render_template('index.html', title=title)
+
+@main.route('/category/pitch/new/<category_name>', methods=['GET', 'POST'])
+@login_required
+def new_pitch(category_name):
+    """
+    Function that returns the pitches page.
+    """
+    
+    form = PitchForm()
+    category = category_name
+
+    if form.validate_on_submit():
+        pitch_category = form.category.data
+        pitch = form.pitch.data
+        new_pitch = Pitch(category=pitch_category, pitch=pitch, user=current_user)
+        new_pitch.save_pitch()
+        return redirect(url_for('.category', category_name=category))
+
+    title = 'New Pitch'
+    return render_template('pitches.html', title=title, form=form, category=category)
 
 @main.route('/user/<uname>')
 def profile(uname):
@@ -36,7 +56,7 @@ def update_profile(uname):
     """
     Function that updates the user's profile.
     """
-    
+
     user = User.query.filter_by(username=uname).first()
     if user is None:
         abort(404)
