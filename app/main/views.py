53ca0d4c73132450792edd2c352ee044cc061d48
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-25 16:10:25 +0300

    Add logic for profile route.

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, abort
 from . import main
 from flask_login import login_required
+from ..models import User
 
 # Views
 @main.route('/')
@@ -12,3 +13,16 @@ def index():
     title = 'Home'
 
     return render_template('index.html', title=title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    """
+    Function that returns the user's profile page.
+    """
+
+    user = User.query.filter_by(username=uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user=user)
