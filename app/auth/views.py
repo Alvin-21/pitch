a70034b49eb820b4aceb2bbe87d8a976da49675d
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-26 08:16:14 +0300

    Add logic for sending welcoming email.

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from ..models import User
 from .forms import RegistrationForm, LoginForm
 from .. import db
 from . import auth
+from ..email import mail_message
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -38,6 +39,7 @@ def register():
                     username=form.username.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
+        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
 
     title = "New Account"
--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
+export MAIL_USERNAME=alvinpaul2321@gmail.com
+export MAIL_PASSWORD=Qubzom-nanwy0-tifbyh
+
 python manage.py server
